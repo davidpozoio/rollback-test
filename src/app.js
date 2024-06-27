0eda89b849724f8f9f74937fc1ee4778b9eb31c3
davidpozoio <117756174+davidpozoio@users.noreply.github.com>
2024-06-26 19:13:14 -0500

    Add /users endpoint

--- src/app.js
+++ src/app.js
@@ -2,6 +2,12 @@ const express = require("express");
 
 const app = express();
 
+app.get("/users", (req, res) => {
+  res.status(200).json({
+    users: [],
+  });
+});
+
 app.get("/examples", (req, res) => {
   res.status(200).json({
     examples: [],
