9bc5f62792cf83c325cd869bb92c00b10dd25c58
davidpozoio <117756174+davidpozoio@users.noreply.github.com>
2024-06-26 19:28:17 -0500

    Rollback made

--- src/app.js
+++ src/app.js
@@ -2,18 +2,6 @@ const express = require("express");
 
 const app = express();
 
-app.get("/bears", (req, res) => {
-  res.status(200).json({
-    bears: [],
-  });
-});
-
-app.get("/users", (req, res) => {
-  res.status(200).json({
-    users: [],
-  });
-});
-
 app.get("/examples", (req, res) => {
   res.status(200).json({
     examples: [],
