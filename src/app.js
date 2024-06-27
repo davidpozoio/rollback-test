a08ff4fb0278cccdf43377c32c1d0a50756f5b20
davidpozoio <117756174+davidpozoio@users.noreply.github.com>
2024-06-26 19:26:34 -0500

    Add bears endpoint

--- src/app.js
+++ src/app.js
@@ -2,6 +2,12 @@ const express = require("express");
 
 const app = express();
 
+app.get("/bears", (req, res) => {
+  res.status(200).json({
+    bears: [],
+  });
+});
+
 app.get("/users", (req, res) => {
   res.status(200).json({
     users: [],
